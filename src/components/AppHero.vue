fc05ff0be0d94d70c034288b2281b219bff61f3d
Andrea Fermo <andreafermo.sd@gmail.com>
2023-03-24 17:03:01 +0100

    first commit

---
+++ src/components/AppHero.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1>Che Figata!</h1>
+    </div>
+</template>
+<script>
+
+</script>
+<style scoped>
+h1 {
+    color: gray;
+    font-size: 80px;
+}
+
+div {
+    background-image: url("../../public/dv.webp");
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 400px;
+    width: 900px;
+    margin: auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+}
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>La mia prima app con Vite!</h1>
+</template>
+<script>
+
+</script>
+<style scoped>
+h1 {
+    background-color: #190205;
+    text-align: center;
+    line-height: 75px;
+    color: #bc0318;
+}
+</style>
